5bd8e07949c4528424dffd77104ce2290312cc07
MarvinVN <marvinvnguyen@gmail.com>
2021-10-05 15:30:38 -0400

    edge cases need to be tested, otherwise OK

--- gameState.py
+++ gameState.py
@@ -5,11 +5,33 @@ class gameState:
         if numPlay not in [1, 2, 3, 4]:
             return False #might need to change behavior
         else:
-            self.numPlay = numPlay
+            self.numPlay = numPlay + 1 #plus 1 needed for dealer
             self.players = []
             self.deck = Deck()
-            self.startGame(numPlay)
+            self.startGame(self.numPlay)
 
     def startGame(self, numPlay):
-        for x in range(1, numPlay):
-            self.players[x] = Player(x) #player 0 reserved for dealer, 1-4 for users    
+        if numPlay-1 not in [1, 2, 3, 4]: #again, behavior will need to be changed; num-1 needed for restart
+            return False
+
+        self.players = []
+        for x in range(numPlay):
+            self.players.append(Player(x)) #player 0 reserved for dealer, 1-4 for users
+
+    def restartGame(self, samePlayers):
+        if samePlayers == True:
+            self.deck.build()
+        else:
+            self.numPlay = int(input("How many people are playing?")) + 1
+            self.startGame(self.numPlay)
+
+    def getPlayerHand(self, player):
+        return self.players[player].hand
+
+    def showAllHands(self): #debugging purposes
+        for x in self.players:
+            x.showHand()
+
+    def dealCards(self, num):
+        for x in range(self.numPlay):
+            self.players[x].draw(self.deck, num)
