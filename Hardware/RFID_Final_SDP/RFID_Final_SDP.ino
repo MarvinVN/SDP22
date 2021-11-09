720d7ba15a1842a6abe61da85518b272488d2f01
raycastillo2 <74738343+raycastillo2@users.noreply.github.com>
2021-11-09 18:26:20 -0500

    Update Hardware

---
+++ Hardware/RFID_Final_SDP/RFID_Final_SDP.ino
@@ -0,0 +1,68 @@
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_PN532.h>
+
+#define PN532_SCK  (13)
+#define PN532_MOSI (11)
+#define PN532_SS   (10)
+#define PN532_MISO (12)
+
+#define PN532_IRQ   (2)
+#define PN532_RESET (3)  // Not connected by default on the NFC Shield
+
+Adafruit_PN532 nfc(PN532_SCK, PN532_MISO, PN532_MOSI, PN532_SS);
+
+uint8_t blank = {0x04, 0xF2, 0xB5, 0x9A, 0x77, 0x6B, 0x80};
+
+
+
+void setup(void) {
+  Serial.begin(115200);
+  while (!Serial) delay(10); // for Leonardo/Micro/Zero
+  Serial.println("Starting...");
+  nfc.begin();
+  uint32_t versiondata = nfc.getFirmwareVersion();
+  if (! versiondata) {
+    Serial.print("Didn't find PN53x board");
+    while (1); // halt
+  }
+  // Got ok data, print it out!
+  Serial.print("Found chip PN5"); Serial.println((versiondata>>24) & 0xFF, HEX); 
+  Serial.print("Firmware ver. "); Serial.print((versiondata>>16) & 0xFF, DEC); 
+  Serial.print('.'); Serial.println((versiondata>>8) & 0xFF, DEC);
+  
+  // configure board to read RFID tags
+  nfc.SAMConfig();
+  
+  Serial.println("Begin");
+}
+
+
+void loop(void) {
+  uint8_t success;
+  uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
+  uint8_t uidLength;                        // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
+  int i;  
+  // Wait for an ISO14443A type cards (Mifare, etc.).  When one is found
+  // 'uid' will be populated with the UID, and uidLength will indicate
+  // if the uid is 4 bytes (Mifare Classic) or 7 bytes (Mifare Ultralight)
+  success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
+  
+  if (success) {
+    // Display some basic information about the card
+    Serial.println("Found an ISO14443A card");
+    Serial.print("  UID Length: ");Serial.print(uidLength, DEC);Serial.println(" bytes");
+    Serial.print("  UID Value: ");
+    nfc.PrintHex(uid, uidLength);
+    delay(1000);
+    for (i = 0; i<7; i++){ 
+      if (uid[i] != card[i]){
+        break;
+      }
+      if (i == 6){
+        Serial.print("Blank");
+      }
+    }
+    Serial.println("");
+  }
+}
