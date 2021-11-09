fca57a76b7d438591407067f6ebcaf6e58b315d7
raycastillo2 <74738343+raycastillo2@users.noreply.github.com>
2021-11-09 18:23:05 -0500

    idk

--- RFID_Proto/RFID_Proto.ino
+++ RFID_Proto/RFID_Proto.ino
@@ -7,7 +7,7 @@ constexpr uint8_t SS_PIN = 10;
 MFRC522 mfrc522(SS_PIN, RST_PIN);  
 
 void setup() {
-	Serial.begin(9600);		
+	Serial.begin(115200);		
 	while (!Serial);		
 	SPI.begin();			
 	mfrc522.PCD_Init();		
