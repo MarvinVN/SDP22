ab15ea8e2456a4c5865fc019be974e9daae8db0e
raycastillo2 <74738343+raycastillo2@users.noreply.github.com>
2021-12-05 16:53:06 -0500

    alina do this

--- Hardware/TEST/TEST.ino
+++ Hardware/TEST/TEST.ino
@@ -1,32 +1,15 @@
 #include <Stepper.h>
-// Define number of steps per rotation:
-const int stepsPerRevolution = 2045;
-const int Dispensor = 2;
+//^^ included library
+const int stepsPerRevolution = 2045; //ammount of gear turns (div by 5)
+const int Dispensor = 2;  // pin for dispensot that connects to transistor
+Stepper myStepper = Stepper(stepsPerRevolution, 8, 9, 10, 11); // funciton from the stepper lib
 
-Stepper myStepper = Stepper(stepsPerRevolution, 8, 9, 10, 11);
 void setup() {
-  pinMode (Dispensor, OUTPUT);
+  pinMode (Dispensor, OUTPUT); // sets pinmode
   // max is about 10
-  myStepper.setSpeed(5);
-  Serial.begin(9600);
-  for(int y = 0; y < 2 ; y++){
-     delay(1000);
-     digitalWrite(Dispensor, HIGH);
-     delay(115);
-     digitalWrite(Dispensor, LOW);
-     delay(1000);
-     for(int i = 0; i < 4; i++){
-        myStepper.step(stepsPerRevolution/5);
-        delay(1000);
-        digitalWrite(Dispensor, HIGH);
-        delay(115);
-        digitalWrite(Dispensor, LOW);
-        delay(1000);
-    }
-    myStepper.step(-stepsPerRevolution);
-  }
-  Serial.print("Done Shuffling ");
-  Serial.println("Waiting for input ");
+  digitalWrite(Dispensor, LOW);
+  myStepper.setSpeed(5); // 5 revs per min ~ 12s per cycle
+  Serial.begin(9600);  
 }
 void loop() {
   // 2048 full rotation
@@ -34,21 +17,39 @@ void loop() {
     char playerRequest;
     int playerRequestInt;
     if (Serial.available() > 0) {    // is a character available?
-      playerRequest = Serial.read();
+      playerRequest = Serial.read(); // we read from the serial monitor to see if a input is there
       Serial.print(playerRequest);
       Serial.println();
-      playerRequestInt = playerRequest - '0';
-      if ((playerRequestInt > 0) && (playerRequestInt < 6)){
-        myStepper.step((stepsPerRevolution/5) * (playerRequestInt - 1));
+      playerRequestInt = playerRequest - '0'; //gets the int of the char
+      if ((playerRequestInt > 0) && (playerRequestInt < 6)){ // if within bounds of players
+        myStepper.step((stepsPerRevolution/5) * (playerRequestInt - 1)); // the nuber of 2pi/5 it must rotate
         delay(1000);
         digitalWrite(Dispensor, HIGH);
-        delay(115);
+        delay(515);
         digitalWrite(Dispensor, LOW);
         delay(1000);
-        myStepper.step(-(stepsPerRevolution/5) * (playerRequestInt - 1));
+        myStepper.step(-(stepsPerRevolution/5) * (playerRequestInt - 1)); //where it rotates back to the origin
         Serial.print("Waiting for input: ");
-      } else {
-        return;
+      } else if(playerRequestInt == 0){
+          for(int y = 0; y < 2 ; y++){ // the two dispensor rotations for initial dealing
+             delay(1000); 
+             digitalWrite(Dispensor, HIGH); // we pulse the motor dealing one card
+             delay(215);
+             digitalWrite(Dispensor, LOW);
+             delay(1000);
+             for(int i = 0; i < 4; i++){ // the 5 segments of the circle for players
+                myStepper.step(stepsPerRevolution/5); // rotate 2pi/5 the total gears over 5
+                delay(1000);
+                digitalWrite(Dispensor, HIGH);
+                delay(215);
+                digitalWrite(Dispensor, LOW);
+                delay(1000);
+              }
+            myStepper.step(- (4 * stepsPerRevolution / 5)); // go back to the origin 
       }
+      Serial.println("Waiting for input "); // waits for user input to dispense
     }
+      }else {
+        return;
+      }  
 }
