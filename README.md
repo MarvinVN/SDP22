da0b4c1e8f70fefb9ec65231096968b47d84666d
raycastillo2 <74738343+raycastillo2@users.noreply.github.com>
2021-12-17 00:45:05 -0500

    Starting website

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # SDP22
+
+For website run in terminal
+    cd frontend
+    npm start
+    * this should open a browser so you can see your development
---
+++ Website/frontend/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          SDP 2022
+        </p>
+        <p>
+          Jack Black
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
