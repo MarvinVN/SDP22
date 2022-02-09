5736150a9ddde5b20fe94aa6111c574d06751ce4
raycastillo2 <74738343+raycastillo2@users.noreply.github.com>
2022-02-09 17:39:38 -0500

    Update RFID.c

--- Hardware/AtMega/SDP.X/RFID.c
+++ Hardware/AtMega/SDP.X/RFID.c
@@ -12,75 +12,34 @@
 #include <stdlib.h>
 #include <avr/io.h>
 //#include <Adafruit_PN532.h>
-#define IN1 PB1
-#define IN2 PB2
-#define IN3 PB3
-#define IN4 PB4
 
-#define SET(pin) (PORTB |= _BV(pin))
-#define CLR(pin) (PORTB &= ~_BV(pin))
 
-static int8_t _dir = 1;
-static int8_t _seq = 0;
 
 char playerRequest;
 int playerRequestInt;
 const int Shuffler = 2;
 
-static void stepper_init(void)
-{
-
-    /* set step motor pins as OUTPUT */
-    DDRB |= _BV(IN1)|_BV(IN2)|_BV(IN3)|_BV(IN4);
-}
-
-static void stepper_dir(uint8_t dir)
-{
-    _dir = !!(dir);
-}
-
-static void stepper_move(void)
-{
-
-    _seq = (_seq + (_dir?1:-1)) & 7;
-
-    switch (_seq) {
-    /* Pattern for 8 microsteps: A-AB-B-BC-C-CD-D-DA */
-    /*      [   A   ][   B  ][   C   ][   D   ] */
-    case 0: SET(IN1);CLR(IN2);CLR(IN3);CLR(IN4);break;
-    case 1: SET(IN1);SET(IN2);CLR(IN3);CLR(IN4);break;
-    case 2: CLR(IN1);SET(IN2);CLR(IN3);CLR(IN4);break;
-    case 3: CLR(IN1);SET(IN2);SET(IN3);CLR(IN4);break;
-    case 4: CLR(IN1);CLR(IN2);SET(IN3);CLR(IN4);break;
-    case 5: CLR(IN1);CLR(IN2);SET(IN3);SET(IN4);break;
-    case 6: CLR(IN1);CLR(IN2);CLR(IN3);SET(IN4);break;
-    case 7: SET(IN1);CLR(IN2);CLR(IN3);SET(IN4);break;
-    }
-
-    _delay_ms(1); // 100Hz
-}
-
 
 void main(void) {
     DDRD = 0xFF; // makes port d output
 
-    PORTD |= 0b00000010;   // turns on port pd0
-    _delay_ms(5000);
-    PORTD = 0x00;
-    _delay_ms(5000);        
+    //PORTD |= 0b00000010;   // turns on port pd0
+    //_delay_ms(5000);
+    //PORTD = 0x00;
+    //_delay_ms(5000);        
 
-    uint8_t dir = 1;
-    uint16_t step = 0;
 
-    /* setup */
-    stepper_init();
 
     /* loop */
     while (1) {
-        stepper_dir(dir ^= 1);
-        for (step = 0; step < 4096; ++step) {
-            stepper_move(); // 1 step (1/4096)
-        }
+        PORTB = 0b00000111;
+        PORTB = 0b00000011;
+        PORTB = 0b00001011;
+        PORTB = 0b00001001;
+        PORTB = 0b00001101;
+        PORTB = 0b00001100;
+        PORTB = 0b00001110;
+        PORTB = 0b00000110;
     }
     return;
 }
