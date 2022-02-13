165cf4611ac577b0007e02e4392a28836d0849bc
raycastillo2 <74738343+raycastillo2@users.noreply.github.com>
2022-02-13 15:26:56 -0500

    stuff

---
+++ Hardware/AtMega/SDP.X/build/default/production/RFID.o.d
@@ -0,0 +1,61 @@
+build/default/production/RFID.o.d build/default/production/RFID.o: RFID.c \
+ /Applications/microchip/xc8/v2.32/avr/lib/gcc/avr/5.4.0/include/xc.h \
+ /Applications/microchip/xc8/v2.32/avr/lib/gcc/avr/5.4.0/include/cci.h \
+ /Applications/microchip/xc8/v2.32/avr/avr/include/avr/io.h \
+ /Applications/microchip/xc8/v2.32/avr/avr/include/avr/sfr_defs.h \
+ /Applications/microchip/xc8/v2.32/avr/avr/include/inttypes.h \
+ /Applications/microchip/xc8/v2.32/avr/lib/gcc/avr/5.4.0/include/stdint.h \
+ /Applications/microchip/xc8/v2.32/avr/avr/include/stdint.h \
+ /Users/ray/.mchp_packs/Microchip/ATmega_DFP/2.3.126/xc8/avr/include/avr/iom328p.h \
+ /Applications/microchip/xc8/v2.32/avr/avr/include/avr/portpins.h \
+ /Applications/microchip/xc8/v2.32/avr/avr/include/avr/common.h \
+ /Applications/microchip/xc8/v2.32/avr/avr/include/avr/version.h \
+ /Applications/microchip/xc8/v2.32/avr/avr/include/avr/fuse.h \
+ /Applications/microchip/xc8/v2.32/avr/avr/include/avr/lock.h \
+ /Applications/microchip/xc8/v2.32/avr/avr/include/util/delay.h \
+ /Applications/microchip/xc8/v2.32/avr/avr/include/util/delay_basic.h \
+ /Applications/microchip/xc8/v2.32/avr/avr/include/math.h \
+ /Applications/microchip/xc8/v2.32/avr/avr/include/stdio.h \
+ /Applications/microchip/xc8/v2.32/avr/lib/gcc/avr/5.4.0/include/stdarg.h \
+ /Applications/microchip/xc8/v2.32/avr/lib/gcc/avr/5.4.0/include/stddef.h \
+ /Applications/microchip/xc8/v2.32/avr/avr/include/stdlib.h
+
+/Applications/microchip/xc8/v2.32/avr/lib/gcc/avr/5.4.0/include/xc.h:
+
+/Applications/microchip/xc8/v2.32/avr/lib/gcc/avr/5.4.0/include/cci.h:
+
+/Applications/microchip/xc8/v2.32/avr/avr/include/avr/io.h:
+
+/Applications/microchip/xc8/v2.32/avr/avr/include/avr/sfr_defs.h:
+
+/Applications/microchip/xc8/v2.32/avr/avr/include/inttypes.h:
+
+/Applications/microchip/xc8/v2.32/avr/lib/gcc/avr/5.4.0/include/stdint.h:
+
+/Applications/microchip/xc8/v2.32/avr/avr/include/stdint.h:
+
+/Users/ray/.mchp_packs/Microchip/ATmega_DFP/2.3.126/xc8/avr/include/avr/iom328p.h:
+
+/Applications/microchip/xc8/v2.32/avr/avr/include/avr/portpins.h:
+
+/Applications/microchip/xc8/v2.32/avr/avr/include/avr/common.h:
+
+/Applications/microchip/xc8/v2.32/avr/avr/include/avr/version.h:
+
+/Applications/microchip/xc8/v2.32/avr/avr/include/avr/fuse.h:
+
+/Applications/microchip/xc8/v2.32/avr/avr/include/avr/lock.h:
+
+/Applications/microchip/xc8/v2.32/avr/avr/include/util/delay.h:
+
+/Applications/microchip/xc8/v2.32/avr/avr/include/util/delay_basic.h:
+
+/Applications/microchip/xc8/v2.32/avr/avr/include/math.h:
+
+/Applications/microchip/xc8/v2.32/avr/avr/include/stdio.h:
+
+/Applications/microchip/xc8/v2.32/avr/lib/gcc/avr/5.4.0/include/stdarg.h:
+
+/Applications/microchip/xc8/v2.32/avr/lib/gcc/avr/5.4.0/include/stddef.h:
+
+/Applications/microchip/xc8/v2.32/avr/avr/include/stdlib.h:
