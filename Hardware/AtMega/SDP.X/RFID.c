106a9109991ed9cdf30a4538ba54a2dc6a834d36
raycastillo2 <74738343+raycastillo2@users.noreply.github.com>
2022-03-03 16:55:02 -0500

    update

--- Hardware/AtMega/SDP.X/RFID.c
+++ Hardware/AtMega/SDP.X/RFID.c
@@ -4,7 +4,7 @@
  *
  * Created on February 8, 2022, 12:29 PM
  */
-#define F_CPU 8000000UL	
+#define F_CPU 20000000UL	
 
 #include <xc.h>
 #include "util/delay.h"
@@ -21,11 +21,12 @@ const int Shuffler = 2;
 int period;
 
 
+
 void main(void) {
     DDRD = 0xFF; // makes port d output
     DDRC = 0x0F;
     period = 100;	
-    
+
     
     //PORTD |= 0b00000010;   // turns on port pd0
     //_delay_ms(5000);
@@ -56,5 +57,11 @@ void main(void) {
     }
     PORTC = 0x09;
     _delay_ms(1000);
+    
+    
 }
+
+
+
+
 
