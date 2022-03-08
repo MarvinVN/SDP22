5709ab9449a6cd81ed6c4aaa8c8f0cf9bedc3427
MarvinVN <marvinvnguyen@gmail.com>
2022-03-07 19:32:40 -0500

    .

--- GameCode/test.py
+++ GameCode/test.py
@@ -1,17 +1,23 @@
 import RPi.GPIO as GPIO
 from time import sleep
 
-sleep(2)
-
+pin = 16
 pin_list = [16,20,21] #GPIO pins to be used
 GPIO.setmode(GPIO.BCM)
 
 #initalize all pins in list as output and LOW
-GPIO.setup(pin_list, GPIO.OUT, initial=0)
+GPIO.setup(pin_list, GPIO.OUT, initial=GPIO.LOW)
 
-GPIO.output(16, 1) #set 21 as HIGH
-sleep(1)
-GPIO.output(16, 0) #set 21 as LOW
+GPIO.output(16, GPIO.HIGH) #set as HIGH
+GPIO.output(20, GPIO.HIGH)
+GPIO.output(21, GPIO.HIGH)
+print(GPIO.input(pin))
+sleep(4)
+
+GPIO.output(16, GPIO.LOW) #set as LOW
+GPIO.output(20, GPIO.LOW)
+GPIO.output(21, GPIO.LOW)
+print(GPIO.input(pin))
 sleep(1)
 
 GPIO.cleanup() #reset pins
