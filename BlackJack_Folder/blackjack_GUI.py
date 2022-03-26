ed309c4d3483bf8d1e00838d2d286aa4e74fc03f
alinaxxion <axdaraphet@gmail.com>
2022-03-26 13:44:26 -0400

    .

--- BlackJack_Folder/blackjack_GUI.py
+++ BlackJack_Folder/blackjack_GUI.py
@@ -103,7 +103,7 @@ class Ui_MainWindow(QtCore.QObject):
 
     # SWITCH FROM MAIN WINDOW TO SETTINGS WINDOW
     def mainToSettings(self, current_w):
-        global count
+        global button_counter
         self.timer.stop() # TESTING STOP TIMER
         button_counter += 1 # changing state
         hb.button_press.disconnect() # TESTING DISCONNECTION
