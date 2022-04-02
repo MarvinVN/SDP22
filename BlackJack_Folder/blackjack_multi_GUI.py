76161fa50a15f60c79c1d02fe1c89757c4ce7378
alinaxxion <axdaraphet@gmail.com>
2022-04-02 00:36:12 -0400

    .

--- BlackJack_Folder/blackjack_multi_GUI.py
+++ BlackJack_Folder/blackjack_multi_GUI.py
@@ -88,6 +88,7 @@ db4 = bjb.HWButton(d4)
 sb4 = bjb.HWButton(s4)
 eb4 = bjb.HWButton(e4)
 
+"""
 output_pins = [5, 13, 16, 20, 21]
 GPIO.setup(output_pins, GPIO.OUT, initial=GPIO.LOW)
 
@@ -96,6 +97,7 @@ cs_pin = DigitalInOut(board.D5)
 pn532 = PN532_SPI(spi, cs_pin, debug=False)
 
 pn532.SAM_configuration()
+"""
 
 # BUTTON COUNTER TO KEEP TRACK OF STATE MACHINE
 button_counter = 0
