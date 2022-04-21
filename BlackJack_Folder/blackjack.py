346b9b4ceedb70beceb6740fc682456d44dff805
alinaxxion <axdaraphet@gmail.com>
2022-04-21 01:45:54 -0400

    .

--- BlackJack_Folder/blackjack.py
+++ BlackJack_Folder/blackjack.py
@@ -83,6 +83,7 @@ def blackjack_process(gui_to_bj_queue, bj_to_gui_queue):
         bj_msg = Message("done_round", [gs.players[0].hand, round_score, gs.getWallets(), gs.checkCardCount()])
         bj_to_gui_queue.put(bj_msg)
         #print("sent msg to gui: ", bj_msg[0], bj_msg[1], bj_msg[2], bj_msg[3])
+        print("sent msg to gui: " + bj_msg.id)
 
         # checking game end states
         t2 = time.time()
